6ec1b2c1edbbf26e75ee506a9dde8100d9b05fb0
Madeline-Stark <madeline.stark1@gmail.com>
2018-12-04 12:00:24 -0800

    Add omniauth to sessions/new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,33 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:user][:email])
-    if !@user.authenticate(params[:user][:password])
-        redirect_to login_path
-    else
+    if auth['uid']
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.name = auth['info']['name']
+        u.email = auth['info']['email']
+        u.image = auth['info']['image']
+      end
       session[:user_id] = @user.id
       redirect_to user_path(@user)
+    elsif @user = User.find_by(email: params[:user][:email])
+      if !@user.authenticate(params[:user][:password])
+          redirect_to login_path
+      else
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      end
     end
   end
 
   def destroy
     session.delete :user_id
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
