4b823c28750abe84ef75cd171eabdea58b364d3a
Madeline-Stark <madeline.stark1@gmail.com>
2019-02-11 13:32:31 -0800

    Add document.ready

---
+++ app/assets/javascripts/author.js
@@ -0,0 +1,16 @@
+// * doc ready and eventlistener() - Look at tic tac toe
+// * class Author
+//     * constructors
+//     * newAuthorForm( )
+// * prototypes-research-maybe just capitalize? or change first name to initial?
+// * loadAuthors
+// * loadAuthor
+// * loadBooks
+
+$(document).ready(function() {
+  attachListeners();
+});
+
+function attachListeners() {
+
+}
