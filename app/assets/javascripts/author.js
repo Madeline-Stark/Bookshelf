2c87c4497bdc6c1f83b65953465755964b672749
Madeline-Stark <madeline.stark1@gmail.com>
2019-02-11 13:35:18 -0800

    Sketch out author class

--- app/assets/javascripts/author.js
+++ app/assets/javascripts/author.js
@@ -1,7 +1,4 @@
-// * doc ready and eventlistener() - Look at tic tac toe
-// * class Author
-//     * constructors
-//     * newAuthorForm( )
+
 // * prototypes-research-maybe just capitalize? or change first name to initial?
 // * loadAuthors
 // * loadAuthor
@@ -13,4 +10,22 @@ $(document).ready(function() {
 
 function attachListeners() {
 
+}
+
+class Author {
+	constructor(obj) {
+		this.id = obj.id
+		this.name = obj.name
+		this.books = obj.books
+	}
+
+	static newAuthorForm() {
+		return (`
+		<strong>New artist form</strong>
+			<form>
+				<input id='post-name' type='text' name='name'></input><br>
+				<input type='submit' />
+			</form>
+		`)
+	}
 }
